a87d9add45dbff00152692b2a93351c43163802c
Tracie Weitzman <tlweitzman@gmail.com>
2016-11-04 11:59:56 -0700

    add js recognizing user inputs; at constructor and nearly functional prototype

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,50 @@
 // backend
 function PizzaOrder(size, toppings) {
-  this.size = size;
-  this.toppings = toppings
+  this.size = "";
+  this.toppings = [];
+  this.pizzaCost;
 }
+console.log(PizzaOrder);
 
 PizzaOrder.prototype.cost = function() {
 
+  this.pizzaCost = 12;
+  if (this.size === "Large") {
+    this.pizzaCost += 4;
+  } else if (this.size === "XLarge") {
+    this.pizzaCost += 8;
+  }
+  for (var i = 1; i < this.toppings.length; i ++) {
+    this.pizzaCost += .5;
+  }
+// return pizzaCost;
+  console.log(this.pizzaCost);
+}
+
+PizzaOrder.prototype.receiptStatement = function() {
+  return "Your Pizza is: $" + this.pizzaCost;
 }
 
 // UI
 $(document).ready(function(){
-
-
   $("form#pizza-order").submit(function(event){
     event.preventDefault();
+    var pizzaOrderPlaced = new PizzaOrder;
+    console.log(pizzaOrderPlaced);
 
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      var inputToppings = $(this).val();
-      $("#receipt").append( + "<br>");
+    var inputSize = $("select#size").val();
+    console.log(inputSize);
+
+    var inputToppings = [];
+    $("input:checkbox[name=toppingcheck]:checked").each(function(){
+      inputToppings.push($(this).val());
     });
+    console.log(inputToppings);
 
+    pizzaOrderPlaced.cost();
+    console.log(pizzaOrderPlaced);
+
+    $("#receipt").text(pizzaOrderPlaced.receiptStatement());
 
   });
 });
